2ef70e77d04b23a52acbf57abcfd271e8a53ed0e
rodribatista <rodribatista@icloud.com>
2023-08-21 22:40:38 -0300

    impl auth with keycloak

--- ms-bills/src/main/java/com/example/msbills/controller/BillController.java
+++ ms-bills/src/main/java/com/example/msbills/controller/BillController.java
@@ -4,6 +4,7 @@ import com.example.msbills.models.Bill;
 import com.example.msbills.service.BillService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class BillController {
   private final BillService service;
 
   @GetMapping("/all")
+  @PreAuthorize("hasRole('ROLE_user')")
   public ResponseEntity<List<Bill>> getAll() {
     return ResponseEntity.ok().body(service.getAllBill());
   }
---
+++ ms-bills/src/main/java/com/example/msbills/security/OAuthSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.msbills.security;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .oauth2ResourceServer()
+        .jwt()
+        .jwtAuthenticationConverter(new JwtAuthConverter());
+  }
+
+}
