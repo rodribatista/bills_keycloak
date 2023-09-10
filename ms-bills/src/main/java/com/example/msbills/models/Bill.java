a52949d4427747d5120dfb41435ebd75b29679b9
rodribatista <rodribatista@icloud.com>
2023-09-10 01:13:27 -0300

    specified bills table

--- ms-bills/src/main/java/com/example/msbills/models/Bill.java
+++ ms-bills/src/main/java/com/example/msbills/models/Bill.java
@@ -5,27 +5,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-@Table
+@Table(name = "bills")
 public class Bill {
 
   @Id
   @GeneratedValue(generator = "system-uuid")
   @GenericGenerator(name = "system-uuid", strategy = "uuid2")
   private String idBill;
-
   private String customerBill;
-
   private String productBill;
-
   private Double totalPrice;
 
 }
