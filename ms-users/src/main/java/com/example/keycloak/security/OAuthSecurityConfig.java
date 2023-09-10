28c1c88e1bb77824c7a0fdcfba0b2ac812a8f20a
rodribatista <rodribatista@icloud.com>
2023-09-10 01:55:49 -0300

    impl users ms on system (with security)

---
+++ ms-users/src/main/java/com/example/keycloak/security/OAuthSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.keycloak.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class OAuthSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(
+    HttpSecurity http
+  ) throws Exception {
+    http
+      .authorizeHttpRequests()
+      .anyRequest()
+      .authenticated()
+      .and()
+      .oauth2ResourceServer()
+      .jwt()
+      .jwtAuthenticationConverter(new JwtAuthConverter());
+    return http.build();
+  }
+
+}
