263869cb40a328364dad20c414f712998406d11f
rodribatista <rodribatista@icloud.com>
2023-09-09 23:54:58 -0300

    updated bills init data and schema

--- ms-bills/src/main/resources/data.sql
+++ ms-bills/src/main/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO public.bill (ID_BILL, CUSTOMER_BILL, PRODUCT_BILL, TOTAL_PRICE)
-VALUES (RANDOM_UUID(), 'Jhon', 'courses/Java', 500);
-INSERT INTO public.bill (ID_BILL, CUSTOMER_BILL, PRODUCT_BILL, TOTAL_PRICE)
-VALUES (RANDOM_UUID(), 'Michael', 'courses/Java', 500);
-INSERT INTO public.bill (ID_BILL, CUSTOMER_BILL, PRODUCT_BILL, TOTAL_PRICE)
-VALUES (RANDOM_UUID(), 'Peter', 'courses/Java', 500);
-INSERT INTO public.bill (ID_BILL, CUSTOMER_BILL, PRODUCT_BILL, TOTAL_PRICE)
-VALUES (RANDOM_UUID(), 'Sebastian', 'courses/Java', 500);
-INSERT INTO public.bill (ID_BILL, CUSTOMER_BILL, PRODUCT_BILL, TOTAL_PRICE)
-VALUES (RANDOM_UUID(), 'Aaron', 'courses/Java', 500);
-INSERT INTO public.bill (ID_BILL, CUSTOMER_BILL, PRODUCT_BILL, TOTAL_PRICE)
-VALUES (RANDOM_UUID(), 'William', 'courses/Java', 500);
+INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
+VALUES (RANDOM_UUID(), 'f45ebe98-53ad-418a-95d1-c2cdeb9ccf58', 'courses/Java', 499.99);
+INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
+VALUES (RANDOM_UUID(), 'f45ebe98-53ad-418a-95d1-c2cdeb9ccf58', 'courses/Spring', 799.99);
+INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
+VALUES (RANDOM_UUID(), 'f45ebe98-53ad-418a-95d1-c2cdeb9ccf58', 'courses/SQL', 299.99);
+INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
+VALUES (RANDOM_UUID(), 'e3075c90-b72a-40b7-a52d-a2ae48bb0c0b', 'courses/Java', 499.99);
+INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
+VALUES (RANDOM_UUID(), 'e20c0658-2f24-46cb-abc2-f67768962404', 'courses/Java', 499.99);
+INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
+VALUES (RANDOM_UUID(), 'e20c0658-2f24-46cb-abc2-f67768962404', 'courses/SQL', 299.99);
--- ms-bills/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-create table bill
-(
-    id_bill       varchar(255) not null,
-    customer_bill varchar(255),
-    product_bill  varchar(255),
-    total_price   double,
-    primary key (id_bill)
-);
