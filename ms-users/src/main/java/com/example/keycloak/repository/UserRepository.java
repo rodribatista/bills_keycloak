25ede4942b4348b7b617d4c4e43bc2c064a491b3
rodribatista <rodribatista@icloud.com>
2023-09-09 23:41:15 -0300

    added keycloak admin user service

---
+++ ms-users/src/main/java/com/example/keycloak/repository/UserRepository.java
@@ -0,0 +1,58 @@
+package com.example.keycloak.repository;
+
+import com.example.keycloak.model.User;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import javax.ws.rs.NotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+public class UserRepository {
+
+  private final Keycloak keycloak;
+
+  @Value("${ms-users.keycloak.realm}")
+  private String realm;
+
+  public UserRepository(Keycloak keycloak) {
+    this.keycloak = keycloak;
+  }
+
+  public User findByUsername(String username) {
+    List<UserRepresentation> userRepresentation = keycloak
+      .realm(realm)
+      .users()
+      .searchByUsername(username, true);
+    return userRepresentation.stream()
+      .map(this::toUser)
+      .findFirst()
+      .orElseThrow(NotFoundException::new);
+  }
+
+  public User findByEmail(String email) {
+    List<UserRepresentation> userRepresentation = keycloak
+      .realm(realm)
+      .users()
+      .searchByEmail(email, true);
+    return userRepresentation.stream()
+      .map(this::toUser)
+      .findFirst()
+      .orElseThrow(NotFoundException::new);
+  }
+
+  private User toUser(UserRepresentation userRepresentation) {
+    return User.builder()
+      .id(userRepresentation.getId())
+      .userName(userRepresentation.getUsername())
+      .email(userRepresentation.getEmail())
+      .firstName(userRepresentation.getFirstName())
+      .lastName(userRepresentation.getLastName())
+      .build();
+  }
+
+}
