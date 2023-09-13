d605d8a5eb8dc74e4797d14a20868d40ed23adbc
rodribatista <rodribatista@icloud.com>
2023-09-12 22:17:40 -0300

    update realm-export and inital data

--- ms-bills/src/main/resources/data.sql
+++ ms-bills/src/main/resources/data.sql
@@ -1,12 +1,8 @@
 INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
-VALUES (RANDOM_UUID(), 'f45ebe98-53ad-418a-95d1-c2cdeb9ccf58', 'courses/Java', 499.99);
+VALUES (RANDOM_UUID(), 'id_jhondoe', 'courses/Java', 499.99);
 INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
-VALUES (RANDOM_UUID(), 'f45ebe98-53ad-418a-95d1-c2cdeb9ccf58', 'courses/Spring', 799.99);
+VALUES (RANDOM_UUID(), 'id_jhondoe', 'courses/Spring', 799.99);
 INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
-VALUES (RANDOM_UUID(), 'f45ebe98-53ad-418a-95d1-c2cdeb9ccf58', 'courses/SQL', 299.99);
+VALUES (RANDOM_UUID(), 'id_jhondoe', 'courses/SQL', 299.99);
 INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
-VALUES (RANDOM_UUID(), 'e3075c90-b72a-40b7-a52d-a2ae48bb0c0b', 'courses/Java', 499.99);
-INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
-VALUES (RANDOM_UUID(), 'e20c0658-2f24-46cb-abc2-f67768962404', 'courses/Java', 499.99);
-INSERT INTO public.bills (id_bill, customer_bill, product_bill, total_price)
-VALUES (RANDOM_UUID(), 'e20c0658-2f24-46cb-abc2-f67768962404', 'courses/SQL', 299.99);
+VALUES (RANDOM_UUID(), 'id_janedoe', 'courses/Java', 499.99);
